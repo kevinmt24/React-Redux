117d0407d8e1cba36aed7d261be67a3f0c7ff1b7
Kevin M Thazhathoot <kevin@thazhathoot.com>
2020-03-28 15:55:15 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React , {Component}from 'react';
+import Ninjas from './Navbar';
+import Forms from './Forms'
 
-function App() {
-  return (
+class App extends Component
+{
+  state = {
+    ninjas : [
+      { name : 'Kevin',age: 19, belt:'black',id:1 },
+      { name : 'Ann',age: 20, belt:'orange',id:2 },
+      { name : 'Messi',age: 33, belt:'red',id:3 }
+    ]
+  }
+
+ addNinja = (ninja) => {
+   ninja.id = Math.random();
+   let ninjas = [...this.state.ninjas,ninja];
+   this.setState({
+     ninjas:ninjas  
+   })
+ } 
+
+render(){
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hey There!</h1>
+    <Ninjas ninjas={this.state.ninjas} />
     </div>
-  );
+  )
+}
 }
 
 export default App;
